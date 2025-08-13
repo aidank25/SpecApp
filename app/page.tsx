96eada5face20283ed31ac52bdbe1c3575cb9cf5
Aidan Krenz <ark25@njit.edu>
2025-08-13 16:23:00 -0400

    make canvas visible

--- app/page.tsx
+++ app/page.tsx
@@ -14,16 +14,22 @@ export default function Home() {
       canvasRef.current.width = window.innerWidth;
       canvasRef.current.height = window.innerHeight;
       canvasContextRef.current = canvasRef.current.getContext("2d");
+      if (canvasContextRef.current)
+        canvasContextRef.current.fillStyle = "white";
     }
   }, []);
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <canvas
+          ref={canvasRef}
+          className="w-full h-full bg-white rounded-lg"
+          id="canvas"
+        ></canvas>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <input type="file" id="audioPicker" accept="audio/*" />
           <audio>no support for audio</audio>
-          <canvas id="canvas"></canvas>
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 cursor-default hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             target="_blank"
